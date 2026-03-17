98814aa0b00514fc9754219f231a9f906fa803b9
Ziya Üre <ziyaure06@gmail.com>
2026-03-17 10:30:59 +0300

    feat: implement FlightController skeleton and basic variables v2

--- oyun programlama ödevi/Assets/Scripts/FlightController.cs
+++ oyun programlama ödevi/Assets/Scripts/FlightController.cs
@@ -1,16 +1,33 @@
+// FlightController.cs
+// CENG 454 – HW1: Sky-High Prototype
+// Author: [Your Full Name] | Student ID: [Your ID]
 using UnityEngine;
-
 public class FlightController : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [SerializeField] private float pitchSpeed = 45f; // degrees/second
+    [SerializeField] private float yawSpeed = 45f; // degrees/second
+    [SerializeField] private float rollSpeed = 45f; // degrees/second
+    [SerializeField] private float thrustSpeed = 5f; // units/second
+                                                     // TODO (Task 3-A): Declare a private Rigidbody field named 'rb'
     void Start()
     {
-        
+        // TODO (Task 3-B): Cache GetComponent<Rigidbody>() into 'rb'.
+        // Then set rb.freezeRotation = true.
+        // Why is freezeRotation needed? Answer in your PDF.
     }
-
-    // Update is called once per frame
-    void Update()
+    void Update()// or FixedUpdate()
     {
-        
+        HandleRotation();
+        HandleThrust();
+    }
+    private void HandleRotation()
+    {
+        // TODO (Task 3-C):
+        // Pitch
+        // Roll
+    }
+    private void HandleThrust()
+    {
+        // TODO (Task 3-D):
     }
 }
